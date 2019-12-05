95051a72081a98fedaf896800ba17d7b6008db66
Aejaz111750 <aejazaejaz@outlook.com>
2019-12-05 10:08:17 +0530

    Add files via upload

--- server.R
+++ server.R
@@ -5,19 +5,106 @@ shinyServer(function(input, output, session) {
       return(NULL)
     }
     else{
-      document <- readLines(input$inputFile$datapath)
-      doc.id <- seq(1:length(document))
-      calid <- data.frame(doc.id, document)
-      #tokens <- tokenize_sentences(document)
-      return(calid)
+      document <- readLines(input$inputFile$datapath, encoding = "latin1")
+      return(document)
     }
   })
-  output$output <- renderPrint({
+  keySet <- reactive({
+    if(is.null(input$keywords)){
+      return(NULL)
+    }
+    else{
+        givenKeys <- input$keywords
+        givenKeys <- givenKeys %>% str_split(',') %>% lapply(str_trim)
+        return(givenKeys)
+    }
+  })
+
+  sentenceArray <- reactive({
+    sentenceArray <- tibble(Document=dataSet()) %>%
+                        mutate(Id=row_number()) %>% select(Id, Document, everything()) %>%
+                        unnest_tokens(Sentences, Document, token="sentences", to_lower=TRUE, drop=FALSE)
+    return(sentenceArray)
+  })
+  
+  filteredSenctences <- reactive({
+    givenKeys <- unlist(keySet())
+    if(length(givenKeys) > 0 ){
+      giveSentences <- sentenceArray()
+      result <- Reduce('|', lapply(givenKeys, function(key) grepl(key, giveSentences$Sentences)))
+      filteredSenctences <- giveSentences[result,, drop=FALSE]$Sentences
+      return(filteredSenctences)
+    }
+    else{
+      return(NULL)
+    }
+  })
+  
+  wordFrequency <- reactive({
+    givenKeys <- unlist(keySet())
+    if(length(givenKeys) > 0 ){
+      bagOfWords <- tibble(Document=dataSet()) %>%
+                      unnest_tokens(Words, Document, to_lower=TRUE)
+      keyWordFreq <- bagOfWords %>% count(Words, sort=TRUE, name='Frequency')
+      keyWordFreq <- keyWordFreq %>% filter(Words %in% givenKeys)
+      return(keyWordFreq)
+    }
+  })
+  
+  relativeFrequency <- reactive({
+    wordFrequency <- wordFrequency()
+    total <- sum(wordFrequency$Frequency)
+    wordFrequency$RelativeFrequency <- (wordFrequency$Frequency /total) * 100
+    relativeFrequency <- wordFrequency[c('Words','RelativeFrequency')]
+    return(relativeFrequency)
+  })
+  
+  output$output <-  renderTable({
     if(is.null(input$inputFile)){
       return(NULL)
     }
     else{
-      dataSet()$calid
+      sentenceArray()
+    }
+  })
+  output$outKeys <- renderText({
+    if(is.null(input$keywords)){
+      return(NULL)
+    }
+    else{
+      toString(unlist(keySet()))
+    }
+  })
+  output$filteredOutput <- renderTable({
+    if(is.null(input$keywords)){
+      return(NULL)
+    }
+    else{
+      filteredSenctences()
+    }
+  })
+  output$keyWordFreqOutput <- renderTable({
+    if(is.null(input$keywords)){
+      return(NULL)
+    }
+    else{
+      relativeFrequency()
+    }
+  })
+  output$relativePlot <- renderPlot({
+    if(is.null(input$keywords)){
+      return(NULL)
+    }
+    else{
+      relativeFrequency() %>% ggplot(aes(x= reorder(Words, -RelativeFrequency), y=RelativeFrequency)) + geom_col() + xlab(NULL)
+    }
+  })
+  output$relativeWordCloud <- renderPlot({
+    if(is.null(input$keywords)){
+      return(NULL)
+    }
+    else{
+      wordcloud(relativeFrequency()$Words, relativeFrequency()$RelativeFrequency, colors=brewer.pal(8, "Dark2"))
     }
   })
 })
--- ui.R
+++ ui.R
@@ -1,7 +1,11 @@
 # Basic Sentence Tokenizer
 
+library(dplyr)
 library(shiny)
-library(tokenizers)
+library(ggplot2)
+library(stringr)
+library(tidytext)
+library(wordcloud)
 
 shinyUI(
   fluidPage(
@@ -9,7 +13,8 @@ shinyUI(
     
     # Sidepane for inputs
     sidebarPanel(
-      fileInput("inputFile", "Upload the text file")
+      fileInput("inputFile", "Upload the text file"),
+      textInput('keywords', 'Keywords', placeholder = "Comma seperated words")
     ),
     
     # Main Panel
@@ -19,8 +24,22 @@ shinyUI(
                            p("To use this app you need a document corpus  in txt file format", align="justify"),
                            verbatimTextOutput("start")),
                   tabPanel("Sentence Tokens", 
-                           verbatimTextOutput("output"))
-                  )
+                           h3(p("All the sentences")),
+                           tableOutput("output"),
+                           h4(p("Given keywords")),
+                           verbatimTextOutput("outKeys")),
+                  tabPanel("Filtered Sentences",
+                           h4(p("Filtered Sentences for given keywords")),
+                           tableOutput("filteredOutput")),
+                  tabPanel("Keyword Frequency", 
+                           h4(p("Frequency of occurrence of keywords in corpus")),
+                           tableOutput("keyWordFreqOutput"),
+                           h4(p("Relative Frequency Bar-Chart")),
+                           plotOutput("relativePlot")),
+                  tabPanel("Keyword Frequency Word Cloud",
+                           h4(p('Word Cloud as per relative frequency')),
+                           plotOutput("relativeWordCloud"))
+      )
     )
   )
 )
