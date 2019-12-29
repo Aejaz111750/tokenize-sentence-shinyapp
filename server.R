eee06aaf7b3f13426a64df985ce1677962df03a9
Aejaz111750 <aejazaejaz@outlook.com>
2019-12-29 09:18:59 +0530

    Add files via upload

--- server.R
+++ server.R
@@ -1,14 +1,82 @@
 # Basic Sentence Tokenizer
 shinyServer(function(input, output, session) {
+  # " Dynamic dropdown div values "
+  colSelector_added = c()
+  # " csv column headers, if .csv file if provided "
+  csvColumnsFromFile <- reactive({
+    if(is.null(input$inputFile)){
+      return(NULL)
+    }
+    else if(!is.null(input$inputFile) && inFileExt()=='csv'){
+      return(names(dataSet()))
+    }
+  })
+  # " CSV column selected from drop down "
+  csvSelectedValue <- reactive({
+    if(!is.null(input$inputFile) && inFileExt()=='csv'){
+      return(c(input[["csvColumns_exists"]]))
+    }
+    else{
+      return(NULL)
+    }
+  })
+  # " Observer for the dynamic field to capture column name "
+  observeEvent(input$inputFile, {
+    if(length(colSelector_added)==0 && inFileExt()=='csv'){
+      ranNum = sample(1:50, 1, replace = FALSE)
+      # id <- "csvColumns"
+      id <- paste0("csvColumns", sep='_', ranNum)
+      insertUI(
+        selector = "#csvColumns",
+        # selector = paste0("#csvColumns", sep='_', ranNum),
+        ui = tags$div(
+          selectInput(
+          # selectizeInput(
+            inputId = paste0("csvColumns",'_exists'),
+            label = 'CSV Columns',
+            choices = csvColumnsFromFile(),
+          ),
+          id = id
+        )
+      )
+      colSelector_added <<- c(id, colSelector_added)
+    }
+    else if (length(colSelector_added)!=0 && inFileExt()=='txt'){
+      removeUI(selector = paste0('#', colSelector_added[length(colSelector_added)]), multiple = TRUE, immediate=TRUE)
+      colSelector_added <<- colSelector_added[-length(colSelector_added)]
+    }
+  })
+  # " Case sensitive flag "
+  isCaseSensitive <- reactive({
+    return(input$isCaseSensitive)
+  })
+  # " File extension of the provided file "
+  inFileExt <- reactive({
+    if(is.null(input$inputFile)){
+      return(NULL)
+    }
+    else{
+      inFileExt <- file_ext(input$inputFile$name)
+      return(inFileExt)
+    }
+  })
+  # " Data from the provided file "
   dataSet <- reactive({
     if(is.null(input$inputFile)){
       return(NULL)
     }
     else{
-      document <- readLines(input$inputFile$datapath, encoding = "latin1")
-      return(document)
+      if(inFileExt()=='txt'){
+        txtData <- readLines(input$inputFile$datapath, encoding = "latin1")
+        return(txtData)
+      }
+      else if (inFileExt()=='csv'){
+        csvData <- read.csv(input$inputFile$datapath)
+        return(csvData)
+      }
     }
   })
+  # " Parsing the given keywords "
   keySet <- reactive({
     if(is.null(input$keywords)){
       return(NULL)
@@ -16,41 +84,84 @@ shinyServer(function(input, output, session) {
     else{
         givenKeys <- input$keywords
         givenKeys <- givenKeys %>% str_split(',') %>% lapply(str_trim)
-        return(givenKeys)
+        if(isCaseSensitive()){
+          return(givenKeys)
+        }
+        else{
+          givenKeys <- givenKeys %>% lapply(tolower)
+          return(givenKeys)
+        }
     }
   })
-
+  # " Tokenizing the corpus to sentences "
   sentenceArray <- reactive({
-    sentenceArray <- tibble(Document=dataSet()) %>%
+    if(inFileExt()=='csv'){
+      if(is.null(csvSelectedValue())){
+        data = NULL
+      }
+      else{
+        data = dataSet()[,csvSelectedValue()]
+      }
+    }
+    else{
+      data = dataSet()
+    }
+    if(isCaseSensitive()){
+      sentenceArray <- tibble(Document=data) %>%
+                          mutate(Id=row_number()) %>% select(Id, Document, everything()) %>%
+                          unnest_tokens(Sentences, Document, token="sentences", to_lower=FALSE, drop=FALSE)
+      return(sentenceArray)
+    }
+    else{
+      sentenceArray <- tibble(Document=data) %>%
                         mutate(Id=row_number()) %>% select(Id, Document, everything()) %>%
                         unnest_tokens(Sentences, Document, token="sentences", to_lower=TRUE, drop=FALSE)
-    return(sentenceArray)
+      return(sentenceArray)
+    }
   })
-  
+  # " Filtering the sentences based on given key words "
   filteredSenctences <- reactive({
     givenKeys <- unlist(keySet())
     if(length(givenKeys) > 0 ){
       giveSentences <- sentenceArray()
-      result <- Reduce('|', lapply(givenKeys, function(key) grepl(key, giveSentences$Sentences)))
+      #result <- Reduce('|', lapply(givenKeys, function(key) grepl(paste0("\\<",key,"\\>"), giveSentences$Sentences, fixed=TRUE)))
+      result <- Reduce('|', lapply(givenKeys, function(key) grepl(key, giveSentences$Sentences, fixed=TRUE)))
       filteredSenctences <- giveSentences[result,, drop=FALSE]$Sentences
-      return(filteredSenctences)
+      return(tibble('Filtered Senctences'=filteredSenctences))
     }
     else{
       return(NULL)
     }
   })
-  
+  # " Preparing the word frequency based on keyword from corpus (word tokenizing) "
   wordFrequency <- reactive({
-    givenKeys <- unlist(keySet())
+      givenKeys <- unlist(keySet())
+    if(inFileExt()=='csv'){
+      if(is.null(csvSelectedValue())){
+        data = NULL
+      }
+      else{
+        data = as.vector(dataSet()[,csvSelectedValue()])
+      }
+    }
+    else{
+      data = dataSet()
+    }
     if(length(givenKeys) > 0 ){
-      bagOfWords <- tibble(Document=dataSet()) %>%
-                      unnest_tokens(Words, Document, to_lower=TRUE)
+      if(isCaseSensitive()){
+        bagOfWords <- tibble(Document=data) %>%
+                        unnest_tokens(Words, Document, to_lower=FALSE)
+      }
+      else{
+        bagOfWords <- tibble(Document=data) %>%
+                        unnest_tokens(Words, Document, to_lower=TRUE)
+      }
       keyWordFreq <- bagOfWords %>% count(Words, sort=TRUE, name='Frequency')
       keyWordFreq <- keyWordFreq %>% filter(Words %in% givenKeys)
       return(keyWordFreq)
     }
   })
-  
+  # " Evaluating the relative frequency "
   relativeFrequency <- reactive({
     wordFrequency <- wordFrequency()
     total <- sum(wordFrequency$Frequency)
@@ -58,9 +169,9 @@ shinyServer(function(input, output, session) {
     relativeFrequency <- wordFrequency[c('Words','RelativeFrequency')]
     return(relativeFrequency)
   })
-  
+  # " Outputs "
   output$output <-  renderTable({
-    if(is.null(input$inputFile)){
+    if(is.null(input$inputFile) || (inFileExt()=='csv' && is.null(csvSelectedValue()))){
       return(NULL)
     }
     else{
@@ -75,8 +186,17 @@ shinyServer(function(input, output, session) {
       toString(unlist(keySet()))
     }
   })
+  output$filteredSentencesCount <- renderText({
+    if(length(unlist(keySet()))==1 && unlist(keySet())==""){
+      return(NULL)
+    }
+    else{
+      nrow(filteredSenctences())
+  }
+  })
   output$filteredOutput <- renderTable({
-    if(is.null(input$keywords)){
+    if(is.null(input$keywords) || (inFileExt()=='csv' && is.null(csvSelectedValue())) || 
+       (length(unlist(keySet()))==1 && unlist(keySet())=="")){
       return(NULL)
     }
     else{
@@ -84,7 +204,7 @@ shinyServer(function(input, output, session) {
     }
   })
   output$keyWordFreqOutput <- renderTable({
-    if(is.null(input$keywords)){
+    if(is.null(input$keywords) || (inFileExt()=='csv' && is.null(csvSelectedValue())) || nrow(relativeFrequency())==0){
       return(NULL)
     }
     else{
@@ -92,7 +212,7 @@ shinyServer(function(input, output, session) {
     }
   })
   output$relativePlot <- renderPlot({
-    if(is.null(input$keywords)){
+    if(is.null(input$keywords) || (inFileExt()=='csv' && is.null(csvSelectedValue())) || nrow(relativeFrequency())==0){
       return(NULL)
     }
     else{
@@ -100,7 +220,8 @@ shinyServer(function(input, output, session) {
     }
   })
   output$relativeWordCloud <- renderPlot({
-    if(is.null(input$keywords)){
+    if(is.null(input$keywords) || (length(unlist(keySet()))==1 && unlist(keySet())=="") || 
+       (inFileExt()=='csv' && is.null(csvSelectedValue())) || nrow(relativeFrequency())==0){
       return(NULL)
     }
     else{
--- ui.R
+++ ui.R
@@ -1,7 +1,8 @@
 # Basic Sentence Tokenizer
-
+#" Required packages for the application "
 library(dplyr)
 library(shiny)
+library(tools)
 library(ggplot2)
 library(stringr)
 library(tidytext)
@@ -9,20 +10,31 @@ library(wordcloud)
 
 shinyUI(
   fluidPage(
-    titlePanel("Basic Sentence Tokenizer"),
+    titlePanel("Keyword Search"),
     
     # Sidepane for inputs
     sidebarPanel(
-      fileInput("inputFile", "Upload the text file"),
-      textInput('keywords', 'Keywords', placeholder = "Comma seperated words")
+      # " Input file for processing "
+      fileInput("inputFile", "Upload the corpus file (txt/csv)", multiple=FALSE, 
+                    accept=c('.csv',
+                             '.txt')),
+      # " Case sensitive check box "
+      checkboxInput("isCaseSensitive", "Case Sensitive Search", value = FALSE),
+      # " Dynamic field based on input file "
+      tags$div(id="csvColumns"),
+      # " Keywords to be searched in corpus "
+      textInput('keywords', 'Keywords', placeholder = "Comma seperated words"),
     ),
     
     # Main Panel
     mainPanel(
       tabsetPanel(type="tabs",
                   tabPanel("Overview", h4(p("How to use this App")),
-                           p("To use this app you need a document corpus  in txt file format", align="justify"),
-                           verbatimTextOutput("start")),
+                           p("Provide a corpus file and keywords to be looked up. 
+                             This app, will filter the sentences for the given keywords (Filtered Sentences),
+                             will evaluate the word frequency from corpus and display them in a word cloud", align="justify"),
+                           # verbatimTextOutput("start")
+                           ),
                   tabPanel("Sentence Tokens", 
                            h3(p("All the sentences")),
                            tableOutput("output"),
@@ -30,6 +42,8 @@ shinyUI(
                            verbatimTextOutput("outKeys")),
                   tabPanel("Filtered Sentences",
                            h4(p("Filtered Sentences for given keywords")),
+                           h5(p("Count of filtered sentences: ")),
+                           verbatimTextOutput("filteredSentencesCount"),
                            tableOutput("filteredOutput")),
                   tabPanel("Keyword Frequency", 
                            h4(p("Frequency of occurrence of keywords in corpus")),
